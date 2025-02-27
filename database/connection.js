c5509daa37fd848518c5f6a5b4c351237de98357
Paul Borrego (Personal) <borregopaulj@gmail.com>
2025-02-27 13:16:43 -0600

    env

--- database/connection.js
+++ database/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Security',
-    database: 'recipe_list'
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 });
 
 
